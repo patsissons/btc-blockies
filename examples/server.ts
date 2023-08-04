d9974ff655424fe4d235308800207053545754bd
Pat Sissons <patrick@goldsky.com>
2023-08-04 10:04:47 -0700

    prettier

--- examples/server.ts
+++ examples/server.ts
@@ -1,9 +1,9 @@
-import Koa from 'koa';
-import serve from 'koa-static';
+import Koa from "koa";
+import serve from "koa-static";
 
 const app = new Koa();
 
-app.use(serve('examples'));
-app.use(serve('src'));
+app.use(serve("examples"));
+app.use(serve("src"));
 
-app.listen(8080, () => console.log('Listening at http://localhost:8080'));
+app.listen(8080, () => console.log("Listening at http://localhost:8080"));
